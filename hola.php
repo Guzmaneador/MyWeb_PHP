3868f4241bad2c4e1d8d725dfe8ea33bdac72d68
munchi <munchi@munchi-PC>
2019-10-24 19:45:02 +0100

    ->Comenzado

---
+++ hola.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+
+<h1>HOLA MUNDO</h1>
+<h2>Adios Mundo</h2>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Caracola</title>
+    </head>
+    <body>
+            <h1>HOLA MUNDO</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
